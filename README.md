e6d7c446e78dc829ef5f476d80c8374d511b49b9
nalbandyan <nalbandyan.inbox@gmail.com>
2024-10-04 16:05:37 +0400

    Fixed 1 bug

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# event-emitter-sync
+# Sync events with emitter to local and remote repo
+
+The implementation of EventHandler (local repo representation) and EventRepository (remote repo representation) is up to you. Main idea is to subscribe to EventEmitter, save it in local stats along with syncing with EventRepository.
+
+The implementation of EventHandler and EventRepository is flexible and left to your discretion. The primary objective is to subscribe to EventEmitter, record the events in `.eventStats`, and ensure synchronization with EventRepository.
+
+The ultimate aim is to have the `.eventStats` of EventHandler and EventRepository have the same values (and equal to the amount of actual events fired by the emitter) by the time MAX_EVENTS have been fired.
+
+## Run
+
+Check the code first, install deps and the `dev` command could help you with running the code,
+another `test` command can give you some context as well.
+
+## Providing results
+
+The best way is to fork this github repo, create pull request with changes and provide a link
+
+## Task completion
+
+### We're expecting
+
+- having equal amount of fired events and handled events
+- equal amount of fired, saved and synced to fake remote repo events every 2 seconds
+- OVERALL RESULTS passed
+
+```ts
+
+// second 1
+----
+Event A: Fired 73 times, In handler 73, In repo 73,
+Event B: Fired 86 times, In handler 86, In repo 86,
+
+// second 2
+----
+Event A: Fired 115 times, In handler 115, In repo // any amount close to or equal to 115,
+Event B: Fired 128 times, In handler 128, In repo // any amount close to or equal to 128,
+
+// results
+
+-–––––- OVERALL RESULTS -–––––-
+Success results passed with 100.00
+Fail results failed with 0.00 (required 0.85)
+
+
+```
---
+++ emitter-sync-pattern.ts
@@ -0,0 +1,93 @@
+/* Check the comments first */
+
+import { EventEmitter } from "./emitter";
+import { EventDelayedRepository } from "./event-repository";
+import { EventStatistics } from "./event-statistics";
+import { ResultsTester } from "./results-tester";
+import { triggerRandomly } from "./utils";
+
+const MAX_EVENTS = 1000;
+
+enum EventName {
+  EventA = "A",
+  EventB = "B",
+}
+
+const EVENT_NAMES = [EventName.EventA, EventName.EventB];
+
+/*
+
+  An initial configuration for this case
+
+*/
+
+function init() {
+  const emitter = new EventEmitter<EventName>();
+
+  triggerRandomly(() => emitter.emit(EventName.EventA), MAX_EVENTS);
+  triggerRandomly(() => emitter.emit(EventName.EventB), MAX_EVENTS);
+
+  const repository = new EventRepository();
+  const handler = new EventHandler(emitter, repository);
+
+  const resultsTester = new ResultsTester({
+    eventNames: EVENT_NAMES,
+    emitter,
+    handler,
+    repository,
+  });
+  resultsTester.showStats(20);
+}
+
+/* Please do not change the code above this line */
+/* ----–––––––––––––––––––––––––––––––––––––---- */
+
+/*
+
+  The implementation of EventHandler and EventRepository is up to you.
+  Main idea is to subscribe to EventEmitter, save it in local stats
+  along with syncing with EventRepository.
+
+  The implementation of EventHandler and EventRepository is flexible and left to your discretion.
+  The primary objective is to subscribe to EventEmitter, record the events in `.eventStats`,
+  and ensure synchronization with EventRepository.
+
+  The ultimate aim is to have the `.eventStats` of EventHandler and EventRepository
+  have the same values (and equal to the actual events fired by the emitter) by the
+  time MAX_EVENTS have been fired.
+
+*/
+
+class EventHandler extends EventStatistics<EventName> {
+  repository: EventRepository;
+
+  constructor(emitter: EventEmitter<EventName>, repository: EventRepository) {
+    super();
+    this.repository = repository;
+
+    emitter.subscribe(EventName.EventA, () => {
+      this.setStats(EventName.EventA, this.getStats(EventName.EventA) + 1);
+      this.repository.saveEventData(EventName.EventA, 1);
+    });
+
+    emitter.subscribe(EventName.EventB, () => {
+      this.setStats(EventName.EventB, this.getStats(EventName.EventB) + 1);
+      this.repository.saveEventData(EventName.EventB, 1);
+    });
+  }
+}
+
+class EventRepository extends EventDelayedRepository<EventName> {
+  // Feel free to edit this class
+
+  async saveEventData(eventName: EventName, _: number) {
+    try {
+      await this.updateEventStatsBy(eventName, 1);
+    } catch (e) {
+      // const _error = e as EventRepositoryError;
+      // console.warn(error);
+    }
+  }
+}
+
+init();
